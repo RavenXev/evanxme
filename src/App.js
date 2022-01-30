c64da3ed87aeade7f3c9104d039fa3e86c76916a
Raven X <ravenxev@gmail.com>
2022-01-30 16:35:41 -0500

    added Articles component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
+import React from 'react';
+import Articles from './Articles'
 
-import {firestore} from '../src/firebase/config'
-import {useState, useEffect} from 'react'
-
-function App() {
-  const [data, setData] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState(false)
-
-  useEffect(() =>{
-    setIsPending(true)
-    firestore.collection('blog-posts').get().then((snapshot)=>{
-      if (snapshot.empty){
-        setError('no blog posts')
-        setIsPending(false)
-      } else {
-        let results = []
-        snapshot.docs.forEach(doc => {
-          results.push({id: doc.id, ...doc.data()})
-        })
-        setData(results)
-        setIsPending(false)
-      }
-    }).catch(err => {
-      setError(err.message)
-      setIsPending(false)
-    })
-
-  }, [])
-
-
+const App = () => {
   return (
-    <div className="App">
-      {error && <p className = 'error'>{error}</p>}
-      {isPending && <p className = 'loading'>Loading...</p>}
-      <ul>
-      {data && data.map((post)=>
-        <li key={post.id}>
-          <h3>{post.title}</h3>
-          <p><i>{post.content}</i></p>
-        </li>
-      )}
-    </ul>
-    </div>
-  );
+    <>
+      <div className="hero">
+        <h1 className = "title">Hello there! I'm Evan Xu.</h1>
+        <h2 className= "subtitle">I think I'm working on something good here, so check it out below :)</h2>
+      </div>
+      <Articles />
+    </>
+  )
 }
 
 export default App;
---
+++ src/Articles.js
@@ -0,0 +1,47 @@
+import {firestore} from '../src/firebase/config'
+import {useState, useEffect} from 'react'
+
+function Articles() {
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() =>{
+    setIsPending(true)
+    firestore.collection('blog-posts').get().then((snapshot)=>{
+      if (snapshot.empty){
+        setError('no blog posts')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({id: doc.id, ...doc.data()})
+        })
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch(err => {
+      setError(err.message)
+      setIsPending(false)
+    })
+
+  }, [])
+
+
+  return (
+    <div>
+      {error && <p className = 'error'>{error}</p>}
+      {isPending && <p className = 'loading'>Loading...</p>}
+      <ul>
+      {data && data.map((post)=>
+        <li key={post.id} className='article'>
+          <h3>{post.title}</h3>
+          <p><i>{post.content}</i></p>
+        </li>
+      )}
+    </ul>
+    </div>
+  );
+}
+
+export default Articles;
