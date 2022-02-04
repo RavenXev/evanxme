35f4dae9f1c418cfbed0468e2c11e83262075a52
Raven X <ravenxev@gmail.com>
2022-02-04 16:27:45 -0500

    started responsive css styling

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./index.css"
 const App = () => {
   return (
     <>
-      <div className="hero">
-        <h1 className = "title">Hello there! I'm Evan Xu.</h1>
+      <div className="hero container">
+        <h1 className = "title">Hello there! <span>I'm Evan Xu.</span></h1>
         <h2 className= "subtitle">I think I'm working on something good here, so check it out below :)</h2>
       </div>
       <Articles />
--- src/Articles.js
+++ src/Articles.js
@@ -1,46 +1,51 @@
-import {firestore} from '../src/firebase/config'
-import {useState, useEffect} from 'react'
-import "./Articles.css"
+import { firestore } from "../src/firebase/config";
+import { useState, useEffect } from "react";
+import "./Articles.css";
 
 function Articles() {
-  const [data, setData] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const [error, setError] = useState(false)
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
 
-  useEffect(() =>{
-    setIsPending(true)
-    firestore.collection('blog-posts').get().then((snapshot)=>{
-      if (snapshot.empty){
-        setError('no blog posts')
-        setIsPending(false)
-      } else {
-        let results = []
-        snapshot.docs.forEach(doc => {
-          results.push({id: doc.id, ...doc.data()})
-        })
-        setData(results)
-        setIsPending(false)
-      }
-    }).catch(err => {
-      setError(err.message)
-      setIsPending(false)
-    })
-
-  }, [])
-
+  useEffect(() => {
+    setIsPending(true);
+    firestore
+      .collection("blog-posts")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("no blog posts");
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <>
-      {error && <p className = 'error'>{error}</p>}
-      {isPending && <p className = 'loading'>Loading...</p>}
-      <ul className='articles-wrapper'>
-      {data && data.map((post)=>
-        <li key={post.id} className='article'>
-          <h3>{post.title}</h3>
-          <p><i>{post.content}</i></p>
-        </li>
-      )}
-    </ul>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      <div className="container">
+        {data &&
+          data.map((post) => (
+            <article key={post.id} className="article">
+              <h3>{post.title}</h3>
+              <p>
+                <i>{post.content}</i>
+              </p>
+            </article>
+          ))}
+      </div>
     </>
   );
 }
