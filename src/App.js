118c1bcfc4081aa17545f58f1029ed36959956e5
Raven X <ravenxev@gmail.com>
2022-02-04 16:49:40 -0500

    switch margin to padding for title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     <>
       <div className="hero container">
         <h1 className = "title">Hello there! <span>I'm Evan Xu.</span></h1>
-        <h2 className= "subtitle">I think I'm working on something good here, so check it out below :)</h2>
+        <h2 className= "subtitle">On a mission to collect and sequence the best mental models for the good life.</h2>
       </div>
       <Articles />
     </>
